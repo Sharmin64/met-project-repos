e0b136a96234ed2cab0deb302c13de6d45c1d0bf
Sharmin64 <lizaeatsbanana@gmail.com>
2024-05-20 22:37:15 +0600

    model updated

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -9,7 +9,7 @@ import {
 const userNameSchema = new Schema<Username>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'Why no First Name?'],
   },
   middleName: {
     type: String,
@@ -69,19 +69,36 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 const studentSchema = new Schema<Student>({
   id: { type: String },
   name: userNameSchema,
-  gender: ['male', 'female'],
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+    required: true,
+  },
   dateOfBirth: { type: String },
   email: { type: String, required: true },
   contactNo: { type: String, required: true },
   emergencyContactNo: { type: String, required: true },
-  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
 
   presentAddress: { type: String, required: true },
   parmanentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  guardian: {
+    type: guardianSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: true,
+  },
   profileImg: { type: String },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 });
 
 export const StudentModel = model<Student>('Student', studentSchema);
